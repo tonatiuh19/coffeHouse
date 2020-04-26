f84c06986df7573c9bb80de90b77f8e76db5c8f6
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2020-04-26 18:49:49 -0500

    favicon

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ session_start();
   <title>CoffeHouse</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 
   <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Monoton&display=swap" rel="stylesheet">
